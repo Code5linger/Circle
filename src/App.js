50634e0eebd836325ddd19a4e83731de02c16a18
Code5linger <sakib.ahmed.1708@gmail.com>
2024-08-09 19:44:03 +0600

    !

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import HeroSection from './component/HeroSection';
+import Navbar from './component/Navbar';
+import './index.css';
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
+}
---
+++ src/component/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <header>
+      <div className="logo">Circle</div>
+      <div className="menu-items">
+        <p>Home</p>
+        <p>Projects</p>
+        <p>Services</p>
+      </div>
+      <div className="menu-btn">Contact Us</div>
+    </header>
+  );
+}
+ 
