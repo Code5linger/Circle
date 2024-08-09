53f702774b08251e5103f1773d9848166d23f286
Code5linger <sakib.ahmed.1708@gmail.com>
2024-08-09 21:34:22 +0600

    ~

--- src/component/HeroSection.js
+++ src/component/HeroSection.js
@@ -2,7 +2,7 @@ import Buttons from './Buttons';
 
 export default function HeroSection() {
   return (
-    <>
+    <div className="hero">
       <div className="container ">
         <div className="hero-text">
           <h1>Empower Your Business With Stunning Design!</h1>
@@ -19,6 +19,19 @@ export default function HeroSection() {
           <img src="./Hero Image.svg" alt="" />
         </div>
       </div>
-    </>
+      <div className="floatItem-A">
+        <div className="floatItem-text">
+          <p>Back</p>
+          <h1>Choose your card</h1>
+        </div>
+        <img src="./Group 51.svg" alt="" />
+      </div>
+      <div className="floatItem-B">
+        <div className="floatItem-text">
+          <p>Project has been accepted.</p>
+          <h1>👍</h1>
+        </div>
+      </div>
+    </div>
   );
 }
